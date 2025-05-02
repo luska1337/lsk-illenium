9fe3f37aaccc31baaede6518befad3ad83436175
Lucas Rodrigues <42743208+luska1337@users.noreply.github.com>
2025-05-02 17:20:02 -0300

    feat(): convert system to illenium-appearance config

---
+++ server/convert.lua
@@ -0,0 +1,176 @@
+Config.Stores = {
+    {
+        type = "clothing",
+        coords = vector4(1693.2, 4828.11, 42.07, 188.66),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false, -- false => uses the size + rotation to create the zone | true => uses points to create the zone
+        showBlip = true, -- overrides the blip visibilty configured above for the group
+        --targetModel = "s_m_m_doctor_01", -- overrides the target ped configured for the group
+        --targetScenario = "" -- overrides the target scenario configure for the group
+        points = {
+            vector3(1686.9018554688, 4829.8330078125, 42.07),
+            vector3(1698.8566894531, 4831.4604492188, 42.07),
+            vector3(1700.2448730469, 4817.7734375, 42.07),
+            vector3(1688.3682861328, 4816.2954101562, 42.07)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(-705.5, -149.22, 37.42, 122),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(-719.86212158203, -147.83151245117, 37.42),
+            vector3(-709.10491943359, -141.53076171875, 37.42),
+            vector3(-699.94342041016, -157.44494628906, 37.42),
+            vector3(-710.68774414062, -163.64665222168, 37.42)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(-1192.61, -768.4, 17.32, 216.6),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(-1206.9552001953, -775.06304931641, 17.32),
+            vector3(-1190.6080322266, -764.03198242188, 17.32),
+            vector3(-1184.5672607422, -772.16949462891, 17.32),
+            vector3(-1199.24609375, -783.07928466797, 17.32)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(425.91, -801.03, 29.49, 177.79),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(419.55020141602, -798.36547851562, 29.49),
+            vector3(431.61773681641, -798.31909179688, 29.49),
+            vector3(431.19784545898, -812.07122802734, 29.49),
+            vector3(419.140625, -812.03594970703, 29.49)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(-168.73, -301.41, 39.73, 238.67),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(-160.82145690918, -313.85919189453, 39.73),
+            vector3(-172.56513977051, -309.82858276367, 39.73),
+            vector3(-166.5775604248, -292.48077392578, 39.73),
+            vector3(-154.84906005859, -296.51647949219, 39.73)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(75.39, -1398.28, 29.38, 6.73),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(81.406135559082, -1400.7791748047, 29.38),
+            vector3(69.335029602051, -1400.8251953125, 29.38),
+            vector3(69.754981994629, -1387.078125, 29.38),
+            vector3(81.500122070312, -1387.3002929688, 29.38)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(-827.39, -1075.93, 11.33, 294.31),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(-826.26251220703, -1082.6293945312, 11.33),
+            vector3(-832.27856445312, -1072.2819824219, 11.33),
+            vector3(-820.16442871094, -1065.7727050781, 11.33),
+            vector3(-814.08953857422, -1076.1878662109, 11.33)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(-1445.86, -240.78, 49.82, 36.17),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(-1448.4829101562, -226.39401245117, 49.82),
+            vector3(-1439.2475585938, -234.70428466797, 49.82),
+            vector3(-1451.5389404297, -248.33193969727, 49.82),
+            vector3(-1460.7554931641, -240.02815246582, 49.82)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(9.22, 6515.74, 31.88, 131.27),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(6.4955291748047, 6522.205078125, 31.88),
+            vector3(14.737417221069, 6513.3872070312, 31.88),
+            vector3(4.3691010475159, 6504.3452148438, 31.88),
+            vector3(-3.5187695026398, 6513.1538085938, 31.88)
+        }
+    },
+    {
+        type = "clothing",
+        coords = vector4(615.35, 2762.72, 42.09, 170.51),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(612.58312988281, 2747.2814941406, 42.09),
+            vector3(612.26214599609, 2767.0520019531, 42.09),
+            vector3(622.37548828125, 2767.7614746094, 42.09),
+            vector3(623.66833496094, 2749.5180664062, 42.09)
+        },
+        job = "police"
+    },
+    {
+        type = "clothing",
+        coords = vector4(1191.61, 2710.91, 38.22, 269.96),
+        size = vector3(4, 4, 4),
+        rotation = 45,
+        usePoly = false,
+        points = {
+            vector3(1188.7923583984, 2704.2021484375, 38.22),
+            vector3(1188.7498779297, 2716.2661132812, 38.22),
+            vector3(1202.4979248047, 2715.8479003906, 38.22),
+            vector3(1202.3558349609, 2703.9294433594, 38.22)
+        },
+        gang = "ballas"
+    },
+}
+
+function Convert()
+    for i = 1, #Config.Stores do
+        local store = Config.Stores[i]
+        local coords, type, gang, job = store.coords, store.type, store.gang, store.job
+        local hasBlip = store.showBlip == true
+        local title = "Shop " .. i
+
+        local formatCoords = string.format("[%.2f,%.2f,%.2f,%.2f]", coords.x, coords.y, coords.z, coords.w)
+
+        local groupType, groupName = gang and "gang" or (job and "job"), gang or job
+        local formatPermission = groupName and json.encode({ [groupType] = groupName }) or nil
+
+        exports.oxmysql:query_async(
+            "INSERT INTO illenium_shops (label, type, coords, blip, groups) VALUES (?, ?, ?, ?, ?)",
+            { title, type, formatCoords, hasBlip, formatPermission }
+        )
+    end
+
+    lib.print.info(locale('CONVERT', #Config.Stores))
+end
+
+-- CreateThread(function()
+--     Wait(100)
+--     Convert()
+-- end)
